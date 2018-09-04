1aebbdf8db87efeaa7069de6364b06cedba3bb6e
duanmuchaoqun <2947824360@qq.com>
2018-09-04 17:24:53 +0800

    上传芒果云企业站demo代码

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\admin\controller;
+
+class Index
+{
+    public function index()
+    {
+        return '这是后台首页';
+    }
+
+    public function test(){
+        return 'Hello world!';
+    }
+}
