0fbe6b7ffc5de5c18cf2866201c7f704204da4ac
duanmuchaoqun <2947824360@qq.com>
2018-09-05 09:13:07 +0800

    修改登录页面

--- public/static/js/cloud.js
+++ public/static/js/cloud.js
@@ -12,9 +12,9 @@
 			$body = $("body");
             $cloud1 = $("#cloud1");
 			$cloud2 = $("#cloud2");
-			
+
             mainwidth = $main.outerWidth();
-         
+
         }
     );
 
@@ -27,15 +27,15 @@
         if (offset2 >= mainwidth) {
 			 offset2 =  -580;
         }
-		
+
         offset1 += 1.1;
 		offset2 += 1;
         $cloud1.css("background-position", offset1 + "px 100px")
-		
+
 		$cloud2.css("background-position", offset2 + "px 460px")
     }, 70);
-	
-	
+
+
 	setInterval(function bg() {
         if (offsetbg >= mainwidth) {
             offsetbg =  -580;
