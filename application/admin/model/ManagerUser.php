07666a03a90d0ceac368f27fd6df464783276470
duanmuchaoqun <2947824360@qq.com>
2018-09-05 15:02:34 +0800

    2018/9/5内容管理系统一期工程 登录功能开发

---
+++ application/admin/model/ManagerUser.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\admin\model;
+
+
+use think\Session;
+
+class ManagerUser extends BaseModel
+{
+    public function userLogin($username = '', $password = '')
+    {
+        $data = $this->where(['username' => $username])->find();
+        if (empty($data)) {
+            return ['code' => 101, 'data' => [], 'msg' => '用户名或者密码错误！！！'];
+        }
+        $encrypt_password = md5($password . $data['salt']);
+        $count = $this->where(['username' => $username, 'password' => $encrypt_password])->count();
+        if ($count <= 0) {
+            return ['code' => 101, 'data' => [], 'msg' => '用户名或者密码错误！！！'];
+        }
+        $managerUser = ['user_name'=>$data['username']];
+        Session::set('manager_user',$managerUser);
+        return ['code' => 100, 'data' => [], 'msg' => '登陆成功！！！'];
+    }
+}
